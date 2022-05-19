0bc8ab3752bb9748496bee1fae713b2543cfb168
GregoryVinicius <gregoryviniciuscla@gmail.com>
2022-05-18 22:07:12 -0300

    Index adicionado

---
+++ lista de exercicios 3.py
@@ -0,0 +1,118 @@
+# ex 1
+# n = int(input("insira um valor para saber se é positivo ou negativo: "))
+
+# if (n > 0):
+#     print("seu valor é positivo:", n)
+# else:
+#     print("seu valor é negativo:", n)
+
+# # ex 2
+# n1 = int(input("insira um numero: "))
+
+# n2 = int(input("insira outro numero: "))
+
+# if (n1 > n2):
+#     print("o primeiro numero é maior:", n1)
+# else:
+#     print("o segundo numero é maior:", n2)
+
+# ex 3
+# idade = int(input("insira sua idade: "))
+
+# if (idade > 18):
+#     print("Entrada permitida")
+# else:
+#     print("Entrada negada")
+
+# ex 4
+# nota_trabalho = int ( input ("insira a nota do trabalho: "))
+
+# nota_prova = int ( input ("insira a nota da sua prova: "))
+
+# media = (nota_trabalho + nota_prova) / 2
+
+# if (media >= 60):
+#     print("Aprovado")
+# else:
+#     print("Reprovado")
+
+# ex 5
+# conceito = input ("escreva seu conceito: ").upper()
+
+# if (conceito == 'A'):
+#     print("aprovado")
+# if (conceito == 'B'):
+#     print ("Aprovado")
+# if (conceito == 'C'):
+#     print ("Aprovado")
+# if (conceito == 'D'):
+#     print("Reprovado")
+
+# ex 6
+# salario = float (input("insira aqui seu salario: "))
+
+# if (salario >= 1212.00):
+#     print("seu salario é igual á", salario / 1212.00, "salarios minimos")
+# else:
+#     print("seu salario é menor a um salario minimo")
+
+# ex 7
+# sexo = input("insira seu sexo: ").upper()
+
+# if (sexo == 'F'):
+#     print("Feminino")
+# else:
+#     print("masculino")
+
+# ex 8
+# idade = int(input("insira sua idade: "))
+
+# if (idade >= 18):
+#     nome = input("insira seu nome: ")
+#     print("Bem vindo", nome)
+# else:
+#     print("Entrada não permitida")
+
+# ex 9 
+
+# idade = int ( input("insira sua idade: "))
+
+# if (idade >= 18):
+#     salario = float (input ("insira seu salario: R$"))
+#     print("seu salario aumentou para: R$", salario + salario * 0.05)
+# else:
+#     print ("Cálculo não permitido")
+
+# ex 10
+
+# numero = int(input("insira um numero: "))
+
+# if(numero >= 0 ):
+#     if(numero <= 100):
+#         print("numero no intervalo permitido")
+#     else:
+#         print("valor fora do intervalo")
+# else:
+#     print("valor fora do intervalo")
+
+# ex 11
+
+# numero = int (input ("insira um numero: "))
+
+# par_impar = numero % 2 
+
+# if(par_impar == 0):
+#     print("Par")
+# else:
+#     print("Impar")
+
+# ex 12
+
+# numero = int (input ("insira um valor: "))
+
+# div = numero % 3 
+
+# if(div == 0):
+#     print("é divisivo por 3")
+# else:
+#     print("não é divisivo por 3")
